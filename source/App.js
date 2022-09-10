f171dd352bac4a631c6ff13fc581c1b60e5755a9
Matrix <1337816495@qq.com>
2022-09-10 13:21:08 +0800

    Update App.js

--- source/App.js
+++ source/App.js
@@ -4,22 +4,30 @@ import React from "react";
 import Header from "./component/Header";
 import Main from "./component/Main";
 
-const chapters = [ "Overview", "Getting Started", "Reference" ];
+const languages = [ "english", "chinese" ];
+const chapters = {
+    [ languages[ 0 ] ]: [ "Overview", "Getting Started", "Reference" ],
+    [ languages[ 1 ] ]: [ "概述", "快速开始", "参考文档" ],
+};
 
 function App () {
 
-    const [ chapter, setChapter ] = React.useState( chapters[ 2 ] );
+    const [ language, setLanguage ] = React.useState( languages[ 0 ] );
+    const [ chapter, setChapter ] = React.useState( chapters[ language ][ 0 ] );
 
     return (
         <>
             <Header
                 chapter={ chapter }
                 chapters={ chapters }
+                language={ language }
                 setChapter={ setChapter }
+                setLanguage={ setLanguage }
             />
             <Main
                 chapter={ chapter }
                 chapters={ chapters }
+                language={ language }
             />
         </>
     );
